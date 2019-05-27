e3d8d0319b5ce68924219ef360b91389d24bfcc4
RahulTewari <RahulTewari@DESKTOP-GO8FSS9>
2019-05-27 10:29:20 +0530

    Commit

---
+++ My_Artificat_Id/src/main/java/com/java/ToPushIntoGit.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class ToPushIntoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, how are you11?");
+	}
+
+}
